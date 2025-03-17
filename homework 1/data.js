89cf4ba09f4aba13bfa267a0f8a02904c8dccbd5
Maxim <androsikmaksim@gmail.com>
2025-03-17 20:50:17 +0300

    add homework 1

---
+++ homework 1/data.js
@@ -0,0 +1,22 @@
+const lessonsData = `
+[
+  {
+    "name": "Mathematical",
+    "time": "10:00-12:00",
+    "maxMembers": 20,
+    "currentMembers": 0
+  },
+  {
+    "name": "Physics",
+    "time": "13:00-14:00",
+    "maxMembers": 21,
+    "currentMembers": 21
+  },
+  {
+    "name": "Chemical",
+    "time": "11:00-12:00",
+    "maxMembers": 25,
+    "currentMembers": 3
+  }
+]
+`;
